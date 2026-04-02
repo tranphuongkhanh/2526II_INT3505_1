a2e175014e4c1dc08a8a7fd958308c36542304df
tranphuongkhanh <161273978+tranphuongkhanh@users.noreply.github.com>
2026-04-02 22:54:00 +0700

    update ver1, ver2
    thêm refreshToken

--- B6/ver2.js
+++ B6/ver2.js
@@ -2,10 +2,13 @@
 // Ở đây có 2 role là admin và reader, chỉ admin mới được thểm, sửa, xóa sách. Reader chỉ được xem sách.
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const SECRET_KEY = 'secret_key_ver1';
+const REFRESH_SECRET_KEY = 'secret_refresh_key';
 const users = [
     { id: 1, username: 'admin', password: '123', role: 'admin' }, 
     { id: 2, username: 'reader', password: '123', role: 'reader' }
@@ -14,15 +17,59 @@ let books = [
     { id: 1, title: 'Clean Code', author: 'Robert C. Martin', status: 'available' },
     { id: 2, title: 'The Pragmatic Programmer', author: 'Andrew Hunt', status: 'borrowed' }
 ];
+let refreshTokens = [];
 
 app.post('/api/login', (req, res) => {
     const user = users.find(u => u.username === req.body.username && u.password === req.body.password);
     if (!user) return res.status(401).json({ message: 'Sai thông tin' });
-    const token = jwt.sign(
+
+    const accessToken = jwt.sign(
         { id: user.id, username: user.username, role: user.role }, 
         SECRET_KEY, 
-        { expiresIn: '1h', algorithm: 'HS256' });
-    res.json({ token });
+        { expiresIn: '1m', algorithm: 'HS256' }
+    );
+
+    const refreshToken = jwt.sign(
+        { id: user.id, username: user.username, role: user.role }, 
+        REFRESH_SECRET_KEY, 
+        { expiresIn: '7d', algorithm: 'HS256' }
+    );
+    refreshTokens.push(refreshToken);
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,// Chống XSS (JavaScript không đọc được)
+        secure: false,// Set = true nếu chạy trên HTTPS
+        sameSite: 'strict', // Chống tấn công CSRF
+        maxAge: 7 * 24 * 60 * 60 * 1000 // Hạn sử dụng: 7 ngày (tính bằng milliseconds)
+    });
+
+    res.json({ accessToken });
+});
+
+app.post('/api/refresh-token', (req, res) => {
+    const token = req.cookies.refreshToken;
+
+    if (!token) return res.status(401).json({ message: 'Thiếu Refresh Token trong Cookie' });
+    if (!refreshTokens.includes(token)) return res.status(403).json({ message: 'Refresh Token không tồn tại trong hệ thống' });
+
+    jwt.verify(token, REFRESH_SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Refresh Token đã hết hạn' });
+
+        const newPayload = { id: user.id, username: user.username, role: user.role };
+        
+        const newAccessToken = jwt.sign(newPayload, SECRET_KEY, { expiresIn: '1m' });
+        
+        res.json({ accessToken: newAccessToken });
+    });
+});
+
+app.post('/api/logout', (req, res) => {
+    const token = req.cookies.refreshToken;
+    
+    refreshTokens = refreshTokens.filter(t => t !== token);
+    res.clearCookie('refreshToken');
+    
+    res.json({ message: 'Đăng xuất thành công, Refresh Token đã bị vô hiệu hóa!' });
 });
 
 const verifyToken = (req, res, next) => {
